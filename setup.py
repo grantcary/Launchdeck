e6d78cff59508a70742b3c9b3aecbc846fc67d37
Grant Cary <85152448+HelloZorex@users.noreply.github.com>
2022-01-03 22:22:43 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 import os
 from setuptools import setup
 
-directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 def parse_requirements(filename):
 	lines = (line.strip() for line in open(filename))
 	return [line for line in lines if line and not line.startswith("#")]
@@ -14,8 +10,6 @@ setup(name='Launchdeck',
     description='Simple Movation Lauchpad button remapping application',
     url='https://github.com/HelloZorex/Launchdeck',
     author='Grant Cary',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     packages=['Launchdeck'],
     install_requires=parse_requirements('requirements.txt'),
     python_requires='>=3.9.6',
