5ac4a656da9c032aba2322978def030d6980c32d
Grant Cary <85152448+grantcary@users.noreply.github.com>
2022-12-26 18:56:50 -0800

    streamlined repeat code

--- src/gui.py
+++ src/gui.py
@@ -27,6 +27,7 @@ class MainWindow(Qw.QWidget):
         self.setLayout(Qw.QVBoxLayout())
         self.topgrid()
         self.button_generator()
+        self.set_tooltip()
         self.show()
 
     def topgrid(self, parent=None):
@@ -43,38 +44,11 @@ class MainWindow(Qw.QWidget):
         self.btn_stop.setToolTip("Stop Launchpad")
         self.btn_stop.setSizePolicy(Qw.QSizePolicy.Expanding, Qw.QSizePolicy.Expanding)
 
-        arr = []
-        arr.append(self.btn_start)
-        arr.append(self.btn_stop)
-        for i in arr:
-            action1 = Qw.QAction("First Action", self)
-
-            self.buttonMenu = Qw.QMenu(i)
-            self.buttonMenu.addAction(action1)
-            self.buttonMenu.addAction("Option 2")
-            self.buttonMenu.addAction("Option 3")
-
-            action1.triggered.connect(lambda: print("clicked"))
-
-            i.installEventFilter(self)
-            self.buttonMenu.installEventFilter(self)
-        
         container2.layout().addWidget(self.btn_start, 0, 1)
         container2.layout().addWidget(self.btn_stop, 0, 2)
 
         self.layout().addWidget(container2, stretch=1)
         
-    def eventFilter(self, source, event):
-        if event.type() == Qt.QEvent.ContextMenu:
-            if source == self.btn_start:
-                self.buttonMenu.exec_(event.globalPos())
-                return True
-            elif source == self.btn_stop:
-                self.buttonMenu.exec_(event.globalPos())
-                return True
-
-        return super().eventFilter(source, event)
-    
     def button_generator(self):
         # https://stackoverflow.com/questions/54927194/python-creating-buttons-in-loop-in-pyqt5
         # https://www.reddit.com/r/learnpython/comments/dvdfp5/add_qpushbuttons_in_loop_with_different_names/
@@ -86,15 +60,41 @@ class MainWindow(Qw.QWidget):
         container.setSizePolicy(Qw.QSizePolicy.Expanding, Qw.QSizePolicy.Expanding)
         container.setStyleSheet("background-color: #171717; padding: 10px;")
 
+        def action_menu(button, idx):
+            action1 = Qw.QAction('Change Tab', self)
+            action1.triggered.connect(lambda ignore, y=idx: self.change_open_tab(y))
+            action2 = Qw.QAction('Change Hotkey', self)
+            action2.triggered.connect(lambda ignore, y=idx: self.change_hot_key(y))
+            action3 = Qw.QAction('Change File', self)
+            action3.triggered.connect(lambda ignore, y=idx: self.change_open_file(y))
+            action4 = Qw.QAction('Change Sound', self)
+            action4.triggered.connect(lambda ignore, y=idx: self.change_play_sound(y))
+            action5 = Qw.QAction('Reset Button', self)
+            action5.triggered.connect(lambda: print('coming soon'))
+
+            buttonMenu = Qw.QMenu(button)
+            buttonMenu.addAction(action1)
+            buttonMenu.addAction(action2)
+            buttonMenu.addAction(action3)
+            buttonMenu.addAction(action4)
+            buttonMenu.addAction(action5)
+
+            button.installEventFilter(self)
+            buttonMenu.installEventFilter(self)
+            return buttonMenu
+
+        # define base variables
         self.prev = None
         self.buttons = {}
+        self.action_events = {}
         spacer = Qw.QSpacerItem(40, 30)
 
-        with open('keymap.json', 'r') as d:
-            hkparse = json.load(d)
-
+        # starts position at 104, ends at 19
+        # the tens place descends, but the ones place ascends
+        # pattern: 48, 49, 31, 32........38, 39, 21, 22, etc
         size = 10
         for btn_row in range(size):
+            # top row seperator
             if btn_row == 1: container.layout().addItem(spacer, 1, 0)
             else:
                 column_cnt = 0
@@ -103,16 +103,35 @@ class MainWindow(Qw.QWidget):
                     num = (tens*10)+ones
                     pos = str(num+3) if (btn_row==0) else str(num)
 
+                    # right side column seperator
                     if btn_column == 8: container.layout().addItem(spacer, btn_row, btn_column)
                     else:
+                        # button 112 is out of bounds, pass over and continue
                         if pos != '112':
                             self.buttons[pos] = Qw.QPushButton(clicked = lambda ignore, x=pos: self.button_select(x))
-                            self.buttons[pos].setToolTip(hkparse[pos][1])
                             container.layout().addWidget(self.buttons[pos], btn_row, btn_column)
+                            self.action_events[pos] = action_menu(self.buttons[pos], pos)
                             column_cnt+=1
                         else: pass
 
         self.layout().addWidget(container, stretch=4)
+
+    def set_tooltip(self):
+        # opens key function dictionary
+        with open('keymap.json', 'r') as d:
+            hkparse = json.load(d)
+
+            for key in self.buttons.keys():
+                self.buttons[key].setToolTip(hkparse[key][1])
+
+    def eventFilter(self, source, event):
+        if event.type() == Qt.QEvent.ContextMenu:
+            if source in self.buttons.values():
+                # checks for button number that matches the source button object id
+                button_key = next((k for k, v in self.buttons.items() if v == source), None)
+                self.action_events[button_key].exec_(event.globalPos())
+                return True
+        return super().eventFilter(source, event)
 
     def button_select(self, text):        
         if self.prev != None and self.prev != text: 
@@ -133,17 +152,33 @@ class MainWindow(Qw.QWidget):
 
         if isinstance(self.bNum, str):
             print(self.bNum)
+        
+    def change_open_file(self, pos):
+        file_filter = "Executable (*.exe)"
+        response, _ = Qw.QFileDialog.getOpenFileName(parent=self, caption="Select File", directory=os.getcwd(), filter=file_filter)
+        rr.openexe(pos, response)
 
-    # def remap_menu(self, self.buttons[text]):        
-    #     self.buttonMenu = Qw.QMenu(self.buttons[text])
-    #     self.buttonMenu.addAction("Option 1") # Open File
-    #     self.buttonMenu.addAction("Option 2") # Play Sound
-    #     self.buttonMenu.addAction("Option 3") # Open Tab
-    #     self.buttonMenu.addAction("Option 4") # Keyboard Shortcut
+    def change_play_sound(self, pos):
+        file_filter = "MP3 File (*.mp3)"
+        response, ok = Qw.QFileDialog.getOpenFileName(parent=self, caption="Select File", directory=os.getcwd(), filter=file_filter)
+        rr.storesound(pos, response)
+        print("Store Sound Stored")
 
-    #     self.buttons[text].installEventFilter(self)
-    #     self.buttonMenu.installEventFilter(self)
+    def change_open_tab(self, pos):
+        url, ok = Qw.QInputDialog.getText(self, "Open Tab", "Enter url:")
+                    
+        if url and ok:
+            rr.opentab(pos, url)
+            print("Finished")
 
+    def change_hot_key(self, pos):
+        shortkey, ok = Qw.QInputDialog.getText(self, "Keyboard Shortcut", "Enter keyboard shortcut:")
+                
+        if shortkey and ok:
+            rr.shortkeys(pos, shortkey)
+            print("Finished")
+
+    # remove this function
     def onActivated(self):
         msg = Qw.QMessageBox()
         msg.setStyleSheet("background-color: #1f1f1f; color: #bababa")
@@ -151,67 +186,12 @@ class MainWindow(Qw.QWidget):
         
         try:
             if isinstance(int(self.bNum), int):
-                taskselect, _ = Qw.QInputDialog.getItem(self, "Select Function", f"Select Function for key {self.bNum}", selectlist, 0, False)
-          
-                if taskselect == "Select Function":
-                    pass
-          
-                elif taskselect == "Open File":
-                    self.getFileDir("1")
-                    print("Finished")
-          
-                elif taskselect == "Play Sound":
-                    self.getSoundFile()
-                    print("Finished")
-          
-                elif taskselect == "Open Tab":
-                    url, ok = Qw.QInputDialog.getText(self, "Open Tab", "Enter url:")
-                    
-                    if url and ok:
-                        rr.opentab(self.bNum, url)
-                        print("Finished")
-                
-                elif taskselect == "Keyboard Shortcut":
-                    shortkey, ok = Qw.QInputDialog.getText(self, "Keyboard Shortcut", "Enter keyboard shortcut:")
-                
-                    if shortkey and ok:
-                        rr.shortkeys(self.bNum, shortkey)
-                        print("Finished")
-            
+               pass
             self.tooltip()
         
         except:
             msg.setText("Select a self.buttons[text] you want to change function of first")
             retval = msg.exec_()
-
-    def settings(self):
-        selectlist = ["Select setting", "Chrome path"]
-        taskselect, _ = Qw.QInputDialog.getItem(self, "Select setting", "Select setting", selectlist)
-        if taskselect == "Chrome path":
-            self.getFileDir("2")
-            print("Finished")
-            
-    def getFileDir(self):
-        file_filter = "Executable (*.exe)"
-        response, _ = Qw.QFileDialog.getOpenFileName(
-            parent=self,
-            caption="Select File",
-            directory=os.getcwd(),
-            filter=file_filter
-        )
-
-        rr.openexe(self.bNum, response)
-
-    def getSoundFile(self):
-        file_filter = "MP3 File (*.mp3)"
-        response, ok = Qw.QFileDialog.getOpenFileName(
-            parent=self,
-            caption="Select File",
-            directory=os.getcwd(),
-            filter=file_filter
-        )
-        rr.storesound(self.bNum, response)
-        print("Store Sound Stored")
 
     def stop(self):
         if self.start_flag == True:
--- src/mapper.py
+++ src/mapper.py
@@ -8,17 +8,17 @@ KEYMAP = 'keymap.json'
 
 def path_reformat(path): return path.replace('\\', '/')
 
-def write_to_file(path, f):
-    with open(path, "w") as d:
-        json.dumps(f, d)
+def write_to_file(map: dict) -> None:
+    with open(KEYMAP, "w") as out_file:
+        json.dump(map, out_file)
 
-def openmap(): return ast.literal_eval(open(KEYMAP).read())
+def openmap() -> ast: return ast.literal_eval(open(KEYMAP).read())
 
 def openexe(key, exepath):
     map = openmap()
     address = path_reformat(exepath)
     map[key] = [str(address), "Open program: " + os.path.basename(address)]
-    write_to_file(KEYMAP, map)
+    write_to_file(map)
 
 def storesound(key, soundpath):
     map = openmap()
@@ -27,18 +27,20 @@ def storesound(key, soundpath):
     
     copyfile(soundpath, f'soundfiles/{filename}')
     map[key] = [str(filename), "Play sound: " + filename]
-    write_to_file(KEYMAP, map)
+    write_to_file(map)
 
 def opentab(key, url):
     map = openmap()
     map[key] = [str(url), "Open tab: " + urlparse(url).netloc]
-    write_to_file(KEYMAP, map)
+    write_to_file(map)
 
 def shortkeys(key, keyshort):
     map = openmap()
     map[key] = [str(keyshort), "Hotkey: " + str(keyshort)]
-    write_to_file(KEYMAP, map)
+    write_to_file(map)
 
 # file = open(KEYMAP,"r+")
 # file.truncate(0)
 # file.close()
+
+opentab('12', 'https://news.ycombinator.com/')
